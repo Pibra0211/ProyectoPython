ef3613d4832692900fdde3ea925ac3508697c979
brajhanrivera96@gmail.com <brajhanrivera@gmail.com>
2026-07-18 18:41:30 -0500

    registro clientes(sin validaciones)

--- clientes.py
+++ clientes.py
@@ -1,16 +1,18 @@
 #funcion registro
+import json 
 datosRegistro = {
     "nombre": "",
     "edad": "",
     "documento":"",
     "tipo":"",
 }
+
 def registroCliente():
     nombre = input("Digite el nombre completo del cliente: ")
     datosRegistro["nombre"] = nombre
     edad = int(input("Digite la edad del cliente: "))
     datosRegistro["edad"] = edad
-    documento = input("Digite el numero de documento del cliente: ")
+    documento = int(input("Digite el numero de documento del cliente: "))
     datosRegistro["documento"] = documento
     tipoVehiculo = input("¿El tipo de vehiculo al que aplica el cliente es carro? s/n ") 
     if tipoVehiculo == "n" or "tipoVehiculo" == "N":
@@ -19,6 +21,6 @@ def registroCliente():
         tipoVehiculo = "carro"
     datosRegistro["tipo"] = tipoVehiculo
     with open("clientess.json","a+") as archivoC:
-        archivoC.write (str(datosRegistro))
+        archivoC.write("\n" + json.dumps(datosRegistro))
     return(datosRegistro)
 print(registroCliente())
