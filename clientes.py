60cbd2f351fedd51722b21d3e82c1321f630c5cf
brajhanrivera96@gmail.com <brajhanrivera@gmail.com>
2026-07-19 10:10:06 -0500

    funcion clientes con todas las validaciones listas

--- clientes.py
+++ clientes.py
@@ -10,38 +10,55 @@ datosRegistro = {
 }
 
 def registroCliente():
-    nombre = input("\nDigite el nombre completo del cliente: ")
+    nombre = input("\nDigite el nombre completo del cliente: ").title().strip() 
     while True:
-        if nombre.isalpha():
+        if nombre.replace(" ","").isalpha():
             break
         else:
-            print("\nEl nombre no puede contener numeros ni caracteres especiales")
-            nombre = input("\nDigite el nombre completo del cliente nuevamente: ").capitalize().strip() 
+            print("\nEl nombre no puede contener numeros ni caracteres especiales.")
+            nombre = input("\nDigite el nombre completo del cliente nuevamente: ").title().strip() 
     datosRegistro["nombre"] = nombre
 
-
-    edad = input("Digite la edad del cliente: ")
+    edad = (input("Digite la edad del cliente: "))
     while True:
         if edad.isdigit() and len(edad) == 2:  
+            edad = int(edad)
+            if (edad) < 16 or (edad) > 60:
+                    edad = input(f"\nEl señor/señora {nombre} debe estar entre los 16 y 60 años\nIngrese la edad nuevamente: ")
+                    continue
             break
-        elif int(edad) < 16:
-              edad = input(f"\nEl señor/señora {nombre} no puede ser menor de 16 años\nIngrese la edad nuevamente:")
         else: edad = input("\nLa edad no puede contener letras ni caracteres especiales y no puede ser mayor a 3 digitos \nIngresese la edad nuevamente: ")
     datosRegistro["edad"] = edad
 
-    documento = int(input("Digite el numero de documento del cliente: "))
+    documento = (input("Digite el numero de documento del cliente: "))
+    while True:
+        if documento.isdigit() and len(documento) > 7:
+            break
+        elif documento.isdigit() and len(documento)  < 10:
+            break
+        else:
+            documento = input("\nEl numero de documento no puede contener letras ni caracteres especiales\nDebe contener 10 digitos, ingreselo nuevamente: ")
     datosRegistro["documento"] = documento
-    telefono = int(input("Digite el numero de telefono del cliente: "))
-    datosRegistro["telefono"] = telefono
+
+    telefono = (input("Digite el numero de telefono del cliente: "))
+    while True:
+        if telefono.isdigit() and len(telefono) == 10:
+            break
+        else:
+            telefono = input("\nEl numero de telefono no puede contener caracteres ni lestas especiales\nIngreselo nuevamente (debe tener 10d igitos): ")
+    datosRegistro["telefono"] = "+57" + telefono
+
     tipoVehiculo = input("¿El tipo de vehiculo al que aplica el cliente es carro? s/n ") 
     if tipoVehiculo == "n" or "tipoVehiculo" == "N":
         tipoVehiculo = "moto"
     elif tipoVehiculo == "s" or "tipoVehiculo" == "S":
         tipoVehiculo = "carro"
     datosRegistro["tipo"] = tipoVehiculo
+    
     with open("clientess.json","a+") as archivoC:
         archivoC.write("\n" + json.dumps(datosRegistro))
     return(datosRegistro)
+
 clientes = (registroCliente())
 print(clientes)
 print(f"\nSeñor/señora {clientes["nombre"]} sus datos se han registrado con exito")
